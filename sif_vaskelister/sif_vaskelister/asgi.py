a0b96eadfef68de45c382d8547fcc01ba000704a
Henrik Hørlück Berg <henrik@horluck.no>
2020-02-12 12:35:47 +0100

    Add .gitignore and Black Python files

--- sif_vaskelister/sif_vaskelister/asgi.py
+++ sif_vaskelister/sif_vaskelister/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sif_vaskelister.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sif_vaskelister.settings")
 
 application = get_asgi_application()
