73d6836d104452459b95a411f4f1406bf6f95036
Aksel Lenes <aksel.lenes@gmail.com>
2020-02-21 12:18:03 +0100

    Add string representation of StudentVillage

--- sif_vaskelister/StudentVillage/models.py
+++ sif_vaskelister/StudentVillage/models.py
@@ -5,6 +5,9 @@ from django.utils.translation import gettext_lazy as _
 class StudentVillage(models.Model):
     name = models.CharField(max_length=80, default="", help_text="Navn på studentby")
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         verbose_name = _("Studenby")
         verbose_name_plural = _("Studentbyer")
