971082756e2cd9da275a61c859d07814ebde3fce
Aksel Lenes <aksel.lenes@gmail.com>
2020-02-21 12:16:12 +0100

    Add name field to StudentVillage

--- sif_vaskelister/StudentVillage/models.py
+++ sif_vaskelister/StudentVillage/models.py
@@ -3,6 +3,8 @@ from django.utils.translation import gettext_lazy as _
 
 
 class StudentVillage(models.Model):
+    name = models.CharField(max_length=80, default="", help_text="Navn på studentby")
+
     class Meta:
         verbose_name = _("Studenby")
         verbose_name_plural = _("Studentbyer")
