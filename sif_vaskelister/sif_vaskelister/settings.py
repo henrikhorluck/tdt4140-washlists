12a5fe2f56a06cc32700af95baa63b36239be074
Henrik Hørlück Berg <henrik@horluck.no>
2020-02-24 14:56:05 +0100

    Add migrations, remove unused scope, make it possible to browse API while logged in

--- sif_vaskelister/sif_vaskelister/settings.py
+++ sif_vaskelister/sif_vaskelister/settings.py
@@ -59,16 +59,11 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-OAUTH2_PROVIDER = {
-    "SCOPES": {
-        "read": "Read scope",
-        "write": "Write scope",
-        "groups": "Access to your groups",
-    }
-}
+OAUTH2_PROVIDER = {"SCOPES": {"read": "Read scope", "write": "Write scope"}}
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.SessionAuthentication",
         "oauth2_provider.contrib.rest_framework.OAuth2Authentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
