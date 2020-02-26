217a4f05d88fcf26e09923b5746d15c6cd788f0a
Egil Tanem <egiltanem@gmail.com>
2020-02-26 17:58:32 +0100

    logikk for vaskeliste

---
+++ frontend/sif_vaskelister/components/atoms/nav/vaskelister/vaskelsiter.tsx
@@ -0,0 +1,14 @@
+import Reactfrom 'react';
+import './App.css';
+import { useState } from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      
+      <h1> </h1>
+    </div>
+  );
+}
+
+export default App;
