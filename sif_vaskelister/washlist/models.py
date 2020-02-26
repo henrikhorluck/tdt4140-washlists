157b69aea406b8504f51ba29535b0106bc2b54d0
akselsl <akselsl@stud.ntnu.no>
2020-02-26 17:09:03 +0100

    Add a OneToOne relation between a washlist and a dorm-room
    This relation is for the wash list that keeps track of the cleaning
    state of a single dorm room.

--- sif_vaskelister/washlist/models.py
+++ sif_vaskelister/washlist/models.py
@@ -4,7 +4,9 @@ from Dormroom.models import Dormroom
 
 
 class WashList(models.Model):
-    title = models.CharField(max_length=250, default="")    
+    title = models.CharField(max_length=250, default="")
+    dorm_room = models.OneToOneField(Dormroom, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.title
 
