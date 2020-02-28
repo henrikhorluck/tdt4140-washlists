3fa172c4a123e6e94d88be643754cf0dc04e8aec
Henrik Hørlück Berg <henrik@horluck.no>
2020-02-28 10:56:36 +0100

    Reformat migration

--- sif_vaskelister/StudentVillage/migrations/0003_studentvillage_templatewashlist.py
+++ sif_vaskelister/StudentVillage/migrations/0003_studentvillage_templatewashlist.py
@@ -7,14 +7,19 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('washlist', '0002_auto_20200227_2219'),
-        ('StudentVillage', '0002_auto_20200227_2219'),
+        ("washlist", "0002_auto_20200227_2219"),
+        ("StudentVillage", "0002_auto_20200227_2219"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='studentvillage',
-            name='templateWashList',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='washlist.TemplateWashList'),
-        ),
+            model_name="studentvillage",
+            name="templateWashList",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                to="washlist.TemplateWashList",
+            ),
+        )
     ]
