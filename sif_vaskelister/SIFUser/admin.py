1fa33efe855bf0ac1c8a33d68762fc2b9b58b7a3
Henrik Hørlück Berg <henrik@horluck.no>
2020-03-01 13:08:42 +0100

    Fix Admin-site for user

--- sif_vaskelister/SIFUser/admin.py
+++ sif_vaskelister/SIFUser/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
 
 from .models import User
 
-admin.site.register(User)
+
+class UserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + ((_("Kollektiv"), {"fields": ("dormroom",)}),)
+
+
+admin.site.register(User, UserAdmin)
