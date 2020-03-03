9d5577b301500b47b8a25e502338666ae224a38c
Henrik Hørlück Berg <henrik@horluck.no>
2020-03-03 16:31:20 +0100

    More lint fix

--- frontend/sif_vaskelister/components/api/auth.ts
+++ frontend/sif_vaskelister/components/api/auth.ts
@@ -1,30 +1,33 @@
-import ClientOAuth2, { Token } from 'client-oauth2';
-import { get } from '.';
+import ClientOAuth2, { Token } from "client-oauth2";
+import { get } from ".";
 
 export class AuthUser extends Token {
-    user?: User;
+  user?: User;
 }
 
 export interface User {
-    username: string;
-    email: string;
-    first_name: string;
-    last_name: string;
-    dormrooms: number[];
+  username: string;
+  email: string;
+  first_name: string;
+  last_name: string;
+  dormrooms: number[];
 }
 
 const vaskelisteAuth = new ClientOAuth2({
-    clientId: "a2h2gfMosEXdW0JU9StGow0alae3DCgM8pd4zqDs",
-    clientSecret:
-      "76hDVmHyWfaCfgcBE4pYIMRLkfC7THWdLJkGRIuNSKO4jgPqBWCgcMYE8exuD5ClhUcGzT1C7ELuFyxAnmOsLnhBlKbxd2jwrjpGcS1xI72mU0JXXWHTYpLx88VVuUBw",
-    accessTokenUri: "http://localhost:8000/o/token/",
-    scopes: ["read", "write"]
-  });
+  clientId: "a2h2gfMosEXdW0JU9StGow0alae3DCgM8pd4zqDs",
+  clientSecret:
+    "76hDVmHyWfaCfgcBE4pYIMRLkfC7THWdLJkGRIuNSKO4jgPqBWCgcMYE8exuD5ClhUcGzT1C7ELuFyxAnmOsLnhBlKbxd2jwrjpGcS1xI72mU0JXXWHTYpLx88VVuUBw",
+  accessTokenUri: "http://localhost:8000/o/token/",
+  scopes: ["read", "write"]
+});
 
 export const login = async (username: string, password: string) => {
-    const token: AuthUser = await vaskelisteAuth.owner.getToken(username, password);
-    const VaskeUser = await get<User>("/api/users/1/", {}, { "token": token })
-    token.user = VaskeUser;
-    // Returns object with both user-data and accessTokens    
-    return token;
-}
+  const token: AuthUser = await vaskelisteAuth.owner.getToken(
+    username,
+    password
+  );
+  const VaskeUser = await get<User>("/api/users/1/", {}, { token: token });
+  token.user = VaskeUser;
+  // Returns object with both user-data and accessTokens
+  return token;
+};
