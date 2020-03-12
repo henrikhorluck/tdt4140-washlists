dbd0306b26e2bcbfb7aea31e8b67d5a614efb3f8
akselsl <akselsl@stud.ntnu.no>
2020-03-12 12:10:41 +0100

    Use ListItemsSerializer when serializing items in DormRoomSerializer
    The data presented is cleaner when using the original serializer

--- sif_vaskelister/Dormroom/serializer.py
+++ sif_vaskelister/Dormroom/serializer.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from SIFUser.serializer import UserSerializer
 from StudentVillage.serializer import StudentVillageSerializer
+from washlist.serializer import ListItemSerializer
 
 from .models import Dormroom
 
@@ -9,6 +10,7 @@ from .models import Dormroom
 class DormroomSerializer(serializers.ModelSerializer):
     village = StudentVillageSerializer()
     residents = UserSerializer(many=True)
+    items = ListItemSerializer(many=True)
 
     class Meta:
         model = Dormroom
