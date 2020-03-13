c161644a1efe10297c717b2f8a09ca4ee63dee45
Jonathan Hermansen <hermansenjonathan@gmail.com>
2020-03-13 15:29:02 +0100

    Added separation between manager and user defined tasks ref #28

--- frontend/sif_vaskelister/components/userWashlist/molecules/washlist/Washlist.tsx
+++ frontend/sif_vaskelister/components/userWashlist/molecules/washlist/Washlist.tsx
@@ -18,16 +18,33 @@ const Washlist: FC<Props> = ({ context }) => {
     <div className={styles.list}>
       <h1>VASKELISTE</h1>
       <div className={styles.todo_list}>
+        <p className={styles.desc}>Påkrevde:</p>
         {context.todos && context.todos.items
-          ? context.todos.items.map((todo: any, index: number) => (
-              <WashlistItem
+          ? context.todos.items.map((todo: any, index: number) => {
+            if(todo.template){
+              return (<WashlistItem
                 key={index}
                 id={todo.id}
                 todo={todo}
                 completeTodo={context.completeTodo}
                 // removeTodo={context.removeTodo}
-              />
-            ))
+              />)
+            }
+          })
+          : null}
+          <p className={styles.desc}>Egendefinerte:</p>
+        {context.todos && context.todos.items
+          ? context.todos.items.map((todo: any, index: number) => {
+            if(!todo.template){
+              return (<WashlistItem
+                key={index}
+                id={todo.id}
+                todo={todo}
+                completeTodo={context.completeTodo}
+                // removeTodo={context.removeTodo}
+              />)
+            }
+          })
           : null}
         {/* <TodoAdd addTodo={context.addTodo} /> */}
       </div>
