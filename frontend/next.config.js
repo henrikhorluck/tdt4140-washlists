65c67297fe64c27918080f9acc4166e3d8f8940d
Henrik Hørlück Berg <henrik@horluck.no>
2020-04-16 02:19:43 +0200

    Make Docker-config work
    - Had to fix some dependencies
    - Next.config with ?? isn't supported (yet)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,16 +3,16 @@
 module.exports = () => {
   // For OAuth authentication
   const client_id =
-    process.env.SIF_BACKEND_CLIENT_ID ??
+    process.env.SIF_BACKEND_CLIENT_ID ||
     "SLDhkx2m08A3c357d4WihIWGzcUQ4duc0TeWhUGL";
   const client_secret =
-    process.env.SIF_BACKEND_CLIENT_SECRET ??
+    process.env.SIF_BACKEND_CLIENT_SECRET ||
     "XA1f55cdS3UVmX5bk5Rjb9kqMV4v79a2gATGcvehoXR9BAMkjlda8KA0pTiBtBa2k5LAuxGNTJmQVrp8GaCs7P2IQ146f5KIzFmNOOcNz6sgqIeEYllKyDU2RbcoeFwX";
 
   // For domain-independent application, NOTE: without trailing slash
   const backend_domain =
-    process.env.SIF_BACKEND_DOMAIN ?? "http://localhost:8000";
-  const domain = process.env.SIF_DOMAIN ?? "http://localhost:3000";
+    process.env.SIF_BACKEND_DOMAIN || "http://localhost:8000";
+  const domain = process.env.SIF_DOMAIN || "http://localhost:3000";
 
   return {
 
